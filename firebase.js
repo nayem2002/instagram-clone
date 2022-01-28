c3cce89fcd3eccfc5f0ec13823861213eebbf569
Nayem Ahammad <91801009+nayem2002@users.noreply.github.com>
2022-01-28 06:57:18 -0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCTLWH_zMucsIekDFjD8SAAoyNthLVl-fQ",
-  authDomain: "instagram-clone-bf6f4.firebaseapp.com",
-  projectId: "instagram-clone-bf6f4",
-  storageBucket: "instagram-clone-bf6f4.appspot.com",
-  messagingSenderId: "787680583116",
-  appId: "1:787680583116:web:d0afec3ca8b222ae03d169"
+  apiKey: "AIzaSyBo_jemhnZ90aVta2oOa86TSnYt7egRvro",
+  authDomain: "instagram-clone-5bfc7.firebaseapp.com",
+  projectId: "instagram-clone-5bfc7",
+  storageBucket: "instagram-clone-5bfc7.appspot.com",
+  messagingSenderId: "173518995752",
+  appId: "1:173518995752:web:456c5d6ed8fa16800bd01d"
 };
+
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore();
